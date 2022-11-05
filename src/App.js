a617bf342bd790ebee66aba69668fd00ad08f489
mlfauzi54@gmail.com <mlfauzi54@gmail.com>
2022-11-05 13:28:39 +0700

    not add code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import HeroImg from './components/HeroImg';
+import IconMusic from './components/IconMusic';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <section className='card-summary'>
+        <div className='card-summary__hero-img'>
+          <HeroImg />
+        </div>
+        <div className='container'>
+          <h1 className='card-summary__title'>Order Summary</h1>
+          <p className='card-summary__desc'>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
+          <div className='container-music'>
+            <IconMusic />
+            <div className='container-price'>
+              <h2 className='card-summary__price-name'>Annual Plan</h2>
+              <p className='card-summary__price'>$59.99/year</p>
+            </div>
+            <a className='card-summary__change-price' href='index.html' >Change</a>
+          </div>
+          <button className='card-summary__btn-payment'>Proceed to Payment</button>
+          <a className='card-summary__btn-cancel-order' href='index.html'>Cancel Order</a>
+        </div>
+      </section>
+    </main>
   );
 }
 
